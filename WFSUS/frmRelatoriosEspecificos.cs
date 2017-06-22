351234361b4abe16f0c63457186cf50631e96196
josecarlos271091 <josecarlos271091@gmail.com>
2017-06-22 13:27:03 -0300

    Opcoes de Exportar arquivos em XML
    Opcoes de Exportar arquivos em XML escolhendo o caminho e nome de
    arquivo que o usuario desejar

--- WFSUS/frmRelatoriosEspecificos.cs
+++ WFSUS/frmRelatoriosEspecificos.cs
@@ -146,18 +146,26 @@ namespace WFSUS
 
         private void btnExportar_Click(object sender, EventArgs e)
         {
-            ProducaoBussiness _prod = new ProducaoBussiness();
-            DataTable _dt = _prod.pesquisarExportarProducao(int.Parse(cbUsuario.SelectedValue.ToString()), int.Parse(cbxMes.SelectedValue.ToString()));
-            if (_dt != null)
+
+            SaveFileDialog salvarArquivo = new SaveFileDialog();
+            string _strFormato = DateTime.Now.Date.ToString("yyyyMMdd");
+            salvarArquivo.FileName = "DadosSalvos-" + frmLogin.nomeusuarioLogado + "-" + _strFormato;
+            salvarArquivo.DefaultExt = "*.xml";
+            salvarArquivo.Filter = "Documentos xml (*.xml)|*.xml| Todos os arquivos (*.*)|*.*";
+            if (salvarArquivo.ShowDialog() == System.Windows.Forms.DialogResult.OK && salvarArquivo.FileName.Length > 0)
             {
-                string _strFormato = DateTime.Now.Date.ToString("yyyyMMdd");
-                _dt.WriteXml(@"C:\Users\Jose Carlos\Desktop\PROJETO FINAL TALP\ProjetoFinalTALP\PesquisaSalva-"+frmLogin.nomeusuarioLogado+"-"+_strFormato+".xml");
-                MessageBox.Show("Dados Exportados com Sucesso!", "Exportar", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("Erro ao Exportar XML!", "Exportar", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                limpaTextBox();
+                ProducaoBussiness _prod = new ProducaoBussiness();
+                DataTable _dt = _prod.pesquisarExportarProducao(int.Parse(cbUsuario.SelectedValue.ToString()), int.Parse(cbxMes.SelectedValue.ToString()));
+                if (_dt != null)
+                {
+
+                    _dt.WriteXml(salvarArquivo.FileName);
+                    MessageBox.Show("Dados Exportados com Sucesso!", "Exportar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Erro ao Exportar XML!", "Exportar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
             }
         }
     }
